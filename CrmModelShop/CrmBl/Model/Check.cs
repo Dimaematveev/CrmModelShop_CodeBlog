fc3e5804c399f5b0374f374fbada873af730d637
dimaematveev <dimaematveev@gmail.com>
2020-05-07 22:39:02 +0300

    сделал чек, так как там есть связь с покупателем то делаем связь с покупателем

---
+++ CrmModelShop/CrmBl/Model/Check.cs
@@ -0,0 +1,11 @@
+﻿namespace CrmBl.Model
+{
+    class Check
+    {
+        public int CheckId { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+
+    }
+}
--- CrmModelShop/CrmBl/Model/Customer.cs
+++ CrmModelShop/CrmBl/Model/Customer.cs
@@ -1,9 +1,13 @@
-﻿namespace CrmBl.Model
+﻿using System.Collections.Generic;
+
+namespace CrmBl.Model
 {
     class Customer
     {
         public int CustomerID { get; set; }
         public string Name{ get; set; }
+
+        public virtual ICollection<Check> Checks { get; set; }
 
         public override string ToString()
         {
