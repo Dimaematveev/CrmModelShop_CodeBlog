c549fd38101f7b2ed69d8c663e5c54ab1c32dfd7
dimaematveev <dimaematveev@gmail.com>
2020-05-07 23:25:04 +0300

    Форму для таблицы

--- CrmModelShop/CrmUi/Main.Designer.cs
+++ CrmModelShop/CrmUi/Main.Designer.cs
@@ -28,20 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.сущностиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.товарToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.продавецToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.покупательToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.чекToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сущностиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1211, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // сущностиToolStripMenuItem
+            // 
+            this.сущностиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.товарToolStripMenuItem,
+            this.продавецToolStripMenuItem,
+            this.покупательToolStripMenuItem,
+            this.чекToolStripMenuItem});
+            this.сущностиToolStripMenuItem.Name = "сущностиToolStripMenuItem";
+            this.сущностиToolStripMenuItem.Size = new System.Drawing.Size(91, 24);
+            this.сущностиToolStripMenuItem.Text = "Сущности";
+            // 
+            // товарToolStripMenuItem
+            // 
+            this.товарToolStripMenuItem.Name = "товарToolStripMenuItem";
+            this.товарToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.товарToolStripMenuItem.Text = "Товар";
+            // 
+            // продавецToolStripMenuItem
+            // 
+            this.продавецToolStripMenuItem.Name = "продавецToolStripMenuItem";
+            this.продавецToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.продавецToolStripMenuItem.Text = "Продавец";
+            // 
+            // покупательToolStripMenuItem
+            // 
+            this.покупательToolStripMenuItem.Name = "покупательToolStripMenuItem";
+            this.покупательToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.покупательToolStripMenuItem.Text = "Покупатель";
+            // 
+            // чекToolStripMenuItem
+            // 
+            this.чекToolStripMenuItem.Name = "чекToolStripMenuItem";
+            this.чекToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.чекToolStripMenuItem.Text = "Чек";
+            // 
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1211, 644);
-            this.Name = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem сущностиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem товарToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem продавецToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem покупательToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem чекToolStripMenuItem;
     }
 }
 
