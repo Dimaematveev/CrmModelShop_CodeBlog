8458a2e3384c3c77ab689aa4ca37805fa153b4a6
dimaematveev <dimaematveev@gmail.com>
2020-05-08 12:45:54 +0300

    Сделал изменение из формы Catalog

--- CrmModelShop/CrmUi/Catalog.cs
+++ CrmModelShop/CrmUi/Catalog.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using CrmBl.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,13 +16,119 @@ namespace CrmUi
     public partial class Catalog<T> : Form
         where T:class
     {
-        public Catalog(DbSet<T> set)
+        CrmContext db;
+        DbSet<T> set;
+        public Catalog(DbSet<T> set, CrmContext db)
         {
             InitializeComponent();
-            dataGridView.DataSource = set.Local.ToBindingList();
+
+            this.db = db;
+            this.set = set;
+            this.set.Load();
+            dataGridView.DataSource = this.set.Local.ToBindingList();
+            
         }
 
         private void Catalog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Добавление
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var form = new SellerForm();
+            //if (form.ShowDialog() == DialogResult.OK)
+            //{
+            //    db.Sellers.Add(form.Seller);
+            //    db.SaveChanges();
+            ////}
+            
+            //if (typeof(T)==typeof(Product))
+            //{
+
+            //}
+            //else if (typeof(T) == typeof(Seller))
+            //{
+
+            //}
+            //else if (typeof(T) == typeof(Customer))
+            //{
+
+            //}
+        }
+
+
+        /// <summary>
+        /// РЕдактирование
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var id = dataGridView.SelectedRows[0].Cells[0].Value;
+
+            if (typeof(T) == typeof(Product))
+            {
+                var product = set.Find(id) as Product;
+                if (product != null)
+                {
+                    var form = new ProductForm(product);
+                    if (form.ShowDialog() == DialogResult.OK)
+                    {
+                        product = form.Product;
+                        db.SaveChanges();
+                        dataGridView.Update();
+                    }
+
+                }
+            }
+            else if (typeof(T) == typeof(Seller))
+            {
+                var seller = set.Find(id) as Seller;
+                if (seller != null)
+                {
+                    var form = new SellerForm(seller);
+                    if (form.ShowDialog() == DialogResult.OK)
+                    {
+                        seller = form.Seller;
+                        db.SaveChanges();
+                        dataGridView.Update();
+                    }
+
+                }
+            }
+            else if (typeof(T) == typeof(Customer))
+            {
+                var customer = set.Find(id) as Customer;
+                if (customer != null)
+                {
+                    var form = new CustomerForm(customer);
+                    if (form.ShowDialog() == DialogResult.OK)
+                    {
+                        customer = form.Customer;
+                        db.SaveChanges();
+                        dataGridView.Update();
+                    }
+
+                }
+            }
+        }
+        /// <summary>
+        /// Удаление
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Update(int id, Type type)
         {
 
         }
